152f3750d934c402a703301cac93efcbb9714052
user <user@localhost.localdomain>
2021-02-28 08:07:07 -0500

    deleted login parrot useless script

--- login_parrot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#!/usr/bin/expect -f
-IP="192.168.123.130"
-LEVEL="user"
-PASS=1234;
-
-printf -v STRING "sshpass -p %s ssh -o StrictHostKeyChecking=no %s@%s\n" $PASS $LEVEL $IP;
-
-eval $STRING
